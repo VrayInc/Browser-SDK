0914deaf6b975480d1aa1734cf81d007ebb00d3a
pushpakrajgautam <pushpakrajgautam@gmail.com>
2018-07-27 10:47:28 -0700

    shipping req removed

--- v.0.1/vray-browser-sdk.js
+++ v.0.1/vray-browser-sdk.js
@@ -419,7 +419,6 @@ var VRAY =
            !VRAY.cardHolderName || !VRAY.myVId || 
            !VRAY.phoneNumber || !VRAY.totalAmount ||
            !VRAY.purchaseItem || (VRAY.totalAmount < 0) ||
-           !VRAY.shippingAddr || (VRAY.shippingAddr.length !== 4) ||
            !((VRAY.loginStatus === 0) || (VRAY.loginStatus === 1))) 
         {
             CALLBACK.call("ERROR - Missing required payment information!");
@@ -430,8 +429,6 @@ var VRAY =
         TRANSACTION.init();
         TRANSACTION.deviceType = (mobileDetect.mobile() ? 1 : 0);
         TRANSACTION.loginStatus = parseInt(VRAY.getLoginStatus());
-
-        
 
         var amount = VRAY.totalAmount;
         var purchaseItems = VRAY.purchaseItem;
