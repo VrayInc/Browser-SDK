e65ca41d410c46e0c33114899bab81a61287d6f5
Dan Kwon <dhkwon7740@gmail.com>
2018-08-09 17:04:28 -0700

    Found an error during testing

--- v.0.2/js/paymentservices.js
+++ v.0.2/js/paymentservices.js
@@ -1,4 +1,3 @@
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -189,7 +188,7 @@ var MERCHANT =
 
     setName: function(name)
     {
-        MERCHANT.id = name;
+        MERCHANT.name = name;
     }
 };
 
