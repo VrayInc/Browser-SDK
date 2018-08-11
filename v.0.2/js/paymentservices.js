ae15e0c7c485415c3cd55d5793a18b982d9548ad
pushpakrajgautam <pushpakrajgautam@gmail.com>
2018-08-10 17:51:44 -0700

    key removed again

--- v.0.2/js/paymentservices.js
+++ v.0.2/js/paymentservices.js
@@ -65,8 +65,6 @@ var APPSERVER =
     {
         domainName: 'VRAY Host',
         domainURL: 'https://vraystagingportal.azurewebsites.net',
-        //domainURL   : 'https://vrayserverdevapolo.azurewebsites.net',	
-        macKey: '79aa2cd255bda022e5e0d095eaeea9442800c1fa3c74c85b2a6db2e1f988f952',
 
         getDomainName: function()
         {
