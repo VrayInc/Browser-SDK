b76a87fc99eb0476f36502060138048f67f911a5
Dan Kwon <dhkwon7740@gmail.com>
2018-08-20 17:44:06 -0700

    hmm

--- v.0.2/js/paymentservices.js
+++ v.0.2/js/paymentservices.js
@@ -280,7 +280,7 @@ var PAYMENT =
         {
             window.alert("Payment authorization request: \n\n" + paymentReqParamText + "\n\n");
         }
-        
+        console.log(paymentReqParam.vid);
         if(!UTILS.validVid(paymentReqParam.vid)){
             window.alert("Emaill Address is invalid\n");
         }else {
@@ -2012,7 +2012,8 @@ var UTILS =
     },
 
     validVid: function(input)
-    {
+    {   
+        console.log(input);
         var re = /^(?:[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&amp;'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
         return re.test(input);
     },
