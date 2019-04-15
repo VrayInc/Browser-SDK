94423c04c5209eae8e0cb6641551c005ec6164d1
Gaurav Shah <info@dynamicdreamz.com>
2019-04-15 16:43:21 +0530

    updated chargeservices.js and vray-browser-sdk version

--- js/chargeservices.js
+++ js/chargeservices.js
@@ -33,18 +33,19 @@ function doChargePayment(tid, vid, merchant, token, amount)
             console.log('chargeRequest',chargeRequest); 
             console.log('readyState',this.readyState);
             console.log('REASON',REASON);
+            console.log('responseURL',CALLBACK.paymentResponseURL);
 
             if(CALLBACK.paymentResponseURL){
                 console.log('paymentResponseURL Found');
                 window.location.href = CALLBACK.paymentResponseURL + 
-                                    "?reason=" + reason +
+                                    "?reason=" + REASON.AuthorizationStatus +
                                     "&data=" + data ? data : null + 
                                     "&tid=" + tid;    
             } else {
                 console.log('paymentResponseURL not Found');
                 window.location.href = "https://apps.dynamicdreamz.com/magento/vray/checkout/payment" + 
                                     "?reason=" + REASON.AuthorizationStatus +
-                                    "&data=" + "null" + 
+                                    "&data=" + data ? data : null + 
                                     "&tid=" + tid;
             }
             //chargeResult(REASON.AuthorizationStatus, null, tid)
